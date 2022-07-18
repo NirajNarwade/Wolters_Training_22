b1b4d65b11503d16a03ece9a396f3aa732894421
Niraj Narwade <narwade.niraj.44@gmail.com>
2022-07-18 23:09:17 +0530

    18-07-2022 calculator final

--- JavaScript/programs/calculator/cal.js
+++ JavaScript/programs/calculator/cal.js
@@ -1,3 +1,31 @@
-function add(val){
-    document.getElementById("in1").value = val
+// function add(val){
+//     document.getElementById("in2").value += val
+// }
+
+// function process(val){
+
+//     document.getElementById("in1").value = document.getElementById("in2").value+val
+//     document.getElementById("in2").value = ""
+// }
+// function calculate(){
+
+//     document.getElementById("in1").value += document.getElementById("in2").value
+    
+
+// }
+// This function clear all the values
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+ 
+// This function display values
+function display(value) {
+    document.getElementById("result").value += value;
+}
+ 
+// This function evaluates the expression and returns result
+function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
 }
--- Python/Assignments/patientapp/sqlmanager.py
+++ Python/Assignments/patientapp/sqlmanager.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-conn = sqlite3.connect('patient.db')
+conn = sqlite3.connect('.\Python\Assignments\patientapp\patient.db')
 
 cursor = conn.cursor()
 
