824f7a5731930d70d2b11e1724cce77d38c933fc
Niraj Narwade <narwade.niraj.44@gmail.com>
2022-07-22 10:06:04 +0530

    21-07-2022

--- JavaScript/games/RockPaperScissor/game.js
+++ JavaScript/games/RockPaperScissor/game.js
@@ -2,37 +2,97 @@ var randInt = NaN;
 var round = 0;
 var comp = 0;
 var user = 0;
+var tie = 0;
+var choice1;
+var choice2;
 function buttonHandeler() {
   document.getElementById("btn1").style.display = "none"; 
   document.getElementById("div1").style.display = "block";
 }
 
+function reset(){
+  round = 0;
+  comp = 0;
+  user = 0;
+  tie = 0;
+  display()
+}
+
 function check(ch2) {
+  if(ch2!=1 & ch2!=2 & ch2!=3){
+    document.getElementById("error").innerHTML = "Please, Enter correct input!"
+    document.getElementById("choice").innerHTML = ""
+  }else{
+    game(ch2)
+  }
+}
+
+function choice(){
+  document.getElementById("choice").innerHTML = "Computer Chose: "+choice1+"<br/>You Chose: "+choice2
+}
+
+function selection(ch){
+  if(ch == 1){
+    return "Rock"
+  }else if(ch == 2){
+    return "Paper"
+  }else{
+    return "Scissor"
+  }
+
+}
+
+function game(ch2){
+  document.getElementById("error").innerHTML = ""  
   round = round + 1;
   ch1 = getRandomInt()
-//   document.getElementById("in1").value = "";
-//   document.getElementById("round").innerHTML = "round :" + round;
-//   if (randInt == num) {
-//     document.getElementById("p1").innerHTML =
-//       "Hurrayy! You Guessed correct Number :)\nYou took " +
-//       round +
-//       " this time.";
-//     document.getElementById("btn2").style.display = "none";
-//     document.getElementById("btn3").style.display = "block";
-//     document.getElementById("p1").style.color = "brown";
-//   } else if (num < randInt) {
-//     document.getElementById("p1").innerHTML = "You are close! Guess Higher.";
-//     document.getElementById("p1").style.color = "green";
-//   } else if (num > randInt) {
-//     document.getElementById("p1").innerHTML = "You are close! Guess Lower.";
-//     document.getElementById("p1").style.color = "red";
-//   }
+  choice1 = selection(ch1)
+  choice2 = selection(ch2)
+  choice()
+  if (ch1 == ch2){
+    tie+=1
+    document.getElementById("error").innerHTML = "Oops! It's a tie...Try again" 
+  }else{
+    if(ch1 == 1){
+      if(ch2 == 2){
+        user+=1
+        document.getElementById("error").innerHTML = "Hurrayyy! You won the round!!!" 
+      }else{
+        comp+=1
+        document.getElementById("error").innerHTML = "Damn it! Computer won the round." 
+      }
+    }else if(ch1 ==2){
+      if(ch2 == 3){
+        user+=1
+        document.getElementById("error").innerHTML = "Hurrayyy! You won the round!!!" 
+      }else{
+        comp+=1
+        document.getElementById("error").innerHTML = "Damn it! Computer won the round." 
+        
+      }
+    }else if(ch1 ==3){
+      if(ch2 == 1){
+        user+=1
+        document.getElementById("error").innerHTML = "Hurrayyy! You won the round!!!" 
+      }else{
+        comp+=1
+        document.getElementById("error").innerHTML = "Damn it! Computer won the round." 
+        
+      }
+    }
+  }
+  display()
+  document.getElementById("in1").value = ""
 }
 
 function getRandomInt() {
-    document.getElementById("round").innerHTML = round;
-    document.getElementById("comp").innerHTML = comp;
-    document.getElementById("you").innerHTML = user;
-    document.getElementById("p1").innerHTML = "Computer has chosen";
     return Math.floor(Math.random() * (3 - 1 + 1) + 1);
+}
+
+function display(){
+  document.getElementById("round").innerHTML = round;
+  document.getElementById("comp").innerHTML = comp;
+  document.getElementById("you").innerHTML = user;
+  document.getElementById("tie").innerHTML = tie;
+  document.getElementById("p1").innerHTML = "Computer has made it's choice. Now its your turn";
 }
--- selfstudy/python_CWH/chapter3/Python_OOPS.py
+++ selfstudy/python_CWH/chapter3/Python_OOPS.py
@@ -3,14 +3,62 @@
 # camelCase --> empName
 # class name follows pascalcase
 
-class WkEmp:        #class declaration
-    name = "Niraj"      #variable initialization
+# class WkEmp:        #class declaration
+#     name = "Niraj"      #variable initialization
+#     surname = "Narwade"
+#     def printData(self):        #method declaration
+#         print(f"Name is {self.name}")
+#         print(f"Surname is {self.surname}")
 
-    def printData(self):        #method declaration
-        print(f"Name is {self.surname}")
+# emp1 = WkEmp()      #object instantiation
+# # print(emp1.name)        
+# emp1.surname = "NNN"        #
+# # print(emp1.name)
+# emp1.printData()
 
-emp1 = WkEmp()      #object instantiation
-print(emp1.name)        
-emp1.surname = "Narwade"        #
-print(emp1.name)
-emp1.printData()
+# emp1.name = "ND"        # only affected current instance
+# emp1.printData()
+
+# emp2 = WkEmp()
+# emp2.printData()
+# print("***",WkEmp.name)
+# WkEmp.name = "Simba"        # affects all instances default value
+# print("***",WkEmp.name)
+# emp1.printData()
+# emp2.printData()
+# emp3 = WkEmp()
+# emp3.printData()
+
+# decorator 
+# @staticmethod --> for passing more arguments other than self 
+
+
+#constructor 
+# __init__()
+#  
+# class Emp:
+#     def __init__(self,name,sal):
+#         self.name = name
+#         self.sal = sal
+
+# # niraj = Emp()       #error
+
+# niraj = Emp("Niraj",50000)
+# print(niraj.name,niraj.sal)
+
+#create a class calculator capable to calculate square,cube,sqrt of number
+# import math
+# class Calculator:
+#     def square(self,num):
+#         return num*num
+#     def cube(self,num):
+#         return num*num*num
+#     def sqrt(self,num):
+#         return math.sqrt(num)
+
+
+# cal = Calculator()
+# print(cal.square(2),cal.cube(2),cal.sqrt(2))
+
+#complete and working
+
