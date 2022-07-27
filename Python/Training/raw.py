6182d457f8cb254e915175c4356c4a36714e2859
Niraj Narwade <narwade.niraj.44@gmail.com>
2022-07-27 22:10:14 +0530

    27-07-2022

---
+++ Python/Training/raw.py
@@ -0,0 +1,33 @@
+# Day 1 
+# 21-06-2022
+#   introduction
+#   solid principles - S O done remaining pending
+#   software development life cycle - not clear
+# 
+# Day 2
+# 22-06-2022 
+#   python --> javascript --> java - solid principles
+#   Python - intro - ovwrview
+#   command line python
+#   jupyter
+#   python - variables
+#            datatypes
+#            
+# 
+# Day 3
+# 23-06-2022
+#   datatypes continuation
+#   text,numeric,sequence,mapping,set
+#   string functions
+#   if else
+#   while
+#   for
+#   
+# Day 4
+# 24-06-2022
+#   loop control statements - continue, break, pass
+#   sample code for different types
+#   pycharm git push pull commit
+#   list funtions
+#   function, user defined, with arguments
+#       
