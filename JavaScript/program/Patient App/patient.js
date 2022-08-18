3ffaea72648e47990519c491c14e89f66a25e5db
Niraj Narwade <Niraj.Narwade@wolterskluwer.com>
2022-08-18 09:36:09 +0530

    17-08-2022__Javascript

---
+++ JavaScript/program/Patient App/patient.js
@@ -0,0 +1,48 @@
+// var pname = NaN;
+// var age = NaN;
+// var gender = NaN;
+// var number = NaN;
+
+let patientList = [];
+function addNew(){
+    clearInputs();
+    document.getElementById("patientform").style.display = "block";
+    document.getElementById("patientlist").style.display = "none";
+}
+
+function viewAll(){
+    document.getElementById("patientform").style.display = "none";
+    document.getElementById("patientlist").style.display = "block";
+    document.getElementById("list").innerHTML = "";
+    patientList.forEach(showPatient);
+}
+
+let showPatient = function (p){
+    document.getElementById("list").innerText += "Name : "+`${p.Name}`+"\nAge : "+`${p.Age}`+"\nGender : "+`${p.Gender}`+"\nContact Number : "+`${p.Number}`+"\n\n";
+    
+}
+
+clearInputs = function () {
+    let inputs = document.getElementsByTagName("input");
+    for (let input of inputs) {
+      if (input.getAttribute("type") != "button") input.value = "";
+    }
+  };
+
+
+// function changeHandler(name,value){
+//     console.log(name,value);
+//     // name = value;
+// }
+
+function register(){
+    let patient = {}
+    patient.Name = document.getElementById("pname").value;
+    patient.Age = document.getElementById("age").value;
+    patient.Gender = document.getElementById("gender").value;
+    patient.Number = document.getElementById("number").value;
+    // console.log(patient);
+    patientList.push(patient);
+    // console.log(patientList);
+    viewAll()
+}
---
+++ selfstudy/javascript/JS CWH/basics.js
@@ -0,0 +1,98 @@
+// alert("alert")
+// document.write("document is writing")
+
+// console.log("console log")
+// console.warn("console warn")
+// console.error("console error")
+
+// comments
+// single line
+{/* 
+    multi
+    line
+    comment
+*/}
+
+// variables
+
+// number
+var num = 34;
+// string
+var str = 'hi this is me';
+// object
+var obj = {
+    name: "Niraj",
+    age: 27,
+    city: "Pune"
+}
+// boolean
+var a = true;
+
+// undefined, null, NaN, 
+
+// Datatypes
+// primitive datatype --> undefined, null, number, string, boolean, symbol
+// reference datatype --> array, object
+
+// array
+var arr = [1,2,3,4,5,6];
+arr[0] = 4;
+// console.log(arr[5]);
+
+// arithmetic operators
+// + - * / += -= *= /=
+// logical operators
+//  && || !(logical not)
+
+// functions
+function avg(a,b){
+    return (a+b)/2
+}
+// console.log(avg(2,8))
+
+//  conditionals in js
+{/*
+if (condition){
+    statements
+}
+else if(condition){
+    statements
+}
+else{
+    statements
+}
+*/}
+
+// for( var i=0;i<11;i++){
+//     console.log(i);
+// }
+
+// arr.forEach(function(ele){
+//     console.log(ele)
+// });
+
+let j=0;    // block scope
+const aa = 1;
+while(j<10){
+    console.log(j);
+    j++;
+}
+
+// shift, unshift, tostring, sort
+// length, indexof, lastindexof, slice, replace
+// date functions
+
+// dom and dom manipulation
+
+// events
+// event listener
+
+// arrow functions
+
+// setTimeout(() => { }, timeout);
+
+// local storage - set item, get item
+
+// JSON
+
+
